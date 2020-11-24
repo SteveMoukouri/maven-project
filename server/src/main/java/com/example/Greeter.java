27c99a2eac70bff9ea49838852b1ce235091c29f
SteveMoukouri <35696579+SteveMoukouri@users.noreply.github.com>
2020-11-25 00:22:55 +0100

    Update Greeter.java
    add javadoc

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -7,7 +7,12 @@ public class Greeter {
   public Greeter() {
 
   }
-
+  
+  /**
+ * Affiche un message de salutation.
+ * @param someone  individu 
+ * @return une chaîne formatee
+ */
   public final String greet(final String someone) {
     return String.format("Hello babacar, %s!", someone);
   }
