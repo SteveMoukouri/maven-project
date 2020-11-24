083d6f11574411395aacadc10a4702fd8a157f41
SteveMoukouri <35696579+SteveMoukouri@users.noreply.github.com>
2020-11-25 00:42:00 +0100

    Update Greeter.java
    fix

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,17 +1,19 @@
+/**
+ * Info about this package doing something for package-info.java file.
+ */
 package com.example;
-
-
+/**
+ * Un objet qui sert à saluer.
+ */
 public class Greeter {
-
   /**
  * Constructeur.
  */
   public Greeter() {
   }
-  
   /**
  * Affiche un message de salutation.
- * @param someone  individu 
+ * @param someone individu 
  * @return une chaîne formatee
  */
   public final String greet(final String someone) {
